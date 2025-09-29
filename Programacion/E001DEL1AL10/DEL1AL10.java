6676293726db4ce610caa8e832a36a3b8c3e8d65
balatrower <power333xd@gmail.com>
2025-09-29 12:52:08 +0200

    EJERCICIO DEL 1 AL 10

--- Programacion/E001DEL1AL10/DEL1AL10.java
+++ Programacion/E001DEL1AL10/DEL1AL10.java
@@ -1,10 +1,16 @@
+import java.lang.IO;
+
 public void main(String[]args) {
     for (int i = 1; i <= 10; i++) {
-        IO.printLn(i);
-        dibujarAsteriscos(i);
+        IO.println(i);
+        IO.println(dibujarAsteriscos(i));
     }
 }
 
-public String dibujarAsteriscos() {
-
+public String dibujarAsteriscos(int i) {
+    String cantidadAsteriscos = "";
+    for (int j = 1; j <= i; j++) {
+        cantidadAsteriscos += "*";
+    }
+    return cantidadAsteriscos;
 }
