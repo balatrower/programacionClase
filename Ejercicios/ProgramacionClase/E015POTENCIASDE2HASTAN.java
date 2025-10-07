9b5a07253f914a946f45f373521c73c9cf5ae533
balatrower <power333xd@gmail.com>
2025-10-07 09:56:25 +0200

    E015 HECHO

--- Ejercicios/ProgramacionClase/E015POTENCIASDE2HASTAN.java
+++ Ejercicios/ProgramacionClase/E015POTENCIASDE2HASTAN.java
@@ -4,11 +4,10 @@ public class E015POTENCIASDE2HASTAN {
     public static void main(String[] args) {
         int numeroIteraciones = Integer.parseInt(IO.readln("Hasta que numero quieres clacular potncias: "));
 
-        int baseElegida = Integer.parseInt(IO.readln("Elige en que base quieres calcular las potencias: "));
+        int base = Integer.parseInt(IO.readln("Elige en que base quieres calcular las potencias: "));
 
         for (int i = 0; i < numeroIteraciones + 1; i++) {
             long resultado = 1;
-            int base = baseElegida;
             for (int i1 = 0; i1 < i; i1++) {
                 resultado *= base;
             }
