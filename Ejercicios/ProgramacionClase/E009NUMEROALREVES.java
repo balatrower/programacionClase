1a462cc94b4d10e46ff2516934e633d3347cff16
balatrower <power333xd@gmail.com>
2025-10-19 16:55:58 +0200

    E009 HECHO

--- Ejercicios/ProgramacionClase/E009NUMEROALREVES.java
+++ Ejercicios/ProgramacionClase/E009NUMEROALREVES.java
@@ -1,14 +1,18 @@
-void main() {
-    int numero = Integer.parseInt(IO.readln("Introduce un numero: "));
+package ProgramacionClase;
 
-    String numeroString = String.valueOf(numero);
+public class E009NUMEROALREVES {
+    void main() {
+        int numero = Integer.parseInt(IO.readln("Introduce un numero: "));
 
-    String stringFinal = "";
-    for (int i = 0; i < numeroString.length(); i++) {
-        stringFinal = stringFinal.concat(String.valueOf(numeroString.charAt(numeroString.length() - 1 - i)));
+        String numeroString = String.valueOf(numero);
+
+        String stringFinal = "";
+        for (int i = 0; i < numeroString.length(); i++) {
+            stringFinal = stringFinal.concat(String.valueOf(numeroString.charAt(numeroString.length() - 1 - i)));
+        }
+
+        int numeroFinal = Integer.parseInt(stringFinal);
+
+        System.out.format("Numero invertido: %d", numeroFinal);
     }
-
-    int numeroFinal = Integer.parseInt(stringFinal);
-
-    System.out.format("Numero invertido: %d", numeroFinal);
 }
