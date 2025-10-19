95f276286e22c0d27d0bcdba35f2c59ff7d00b9a
balatrower <power333xd@gmail.com>
2025-10-19 17:12:01 +0200

    E021 HECHO

---
+++ Ejercicios/ProgramacionClase/E021MAYORYMENORDENNUMEROS.java
@@ -0,0 +1,24 @@
+package ProgramacionClase;
+
+public class E021MAYORYMENORDENNUMEROS {
+    void main() {
+        int numeroIteraciones = Integer.parseInt(IO.readln("¿Cuantos numeros desea introducir?"));
+
+        int numeroMayor = 0;
+        int numeroMenor = Integer.MAX_VALUE;
+        IO.println("Introduzca los numeros a continuacion: ");
+        for (int i = 0; i < numeroIteraciones; i++) {
+            int numero = Integer.parseInt(IO.readln());
+
+            if (numero > numeroMayor) {
+                numeroMayor = numero;
+            }
+
+            if (numero < numeroMenor) {
+                numeroMenor = numero;
+            }
+        }
+
+        System.out.format("El menor era %d y el mayor era %d", numeroMenor, numeroMayor);
+    }
+}
