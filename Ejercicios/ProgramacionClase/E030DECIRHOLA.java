0b138667e6e8342adcd581ed1cf7189678e6c07b
balatrower <power333xd@gmail.com>
2025-10-23 23:42:36 +0200

    E030 HECHO

---
+++ Ejercicios/ProgramacionClase/E030DECIRHOLA.java
@@ -0,0 +1,15 @@
+package ProgramacionClase;
+
+public class E030DECIRHOLA {
+    public static class Principal {
+        public static void main(String[] args) {
+            Utilidades.decirHola();
+        }
+    }
+
+    public static class Utilidades {
+        public static void decirHola() {
+            System.out.println("¡Hola!");
+        }
+    }
+}
