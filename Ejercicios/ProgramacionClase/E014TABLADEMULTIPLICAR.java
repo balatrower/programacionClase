fd3f14c6a8b4469f3cc1b6254f3da78ae486a48b
balatrower <power333xd@gmail.com>
2025-10-24 10:15:48 +0200

    E014 hecho

---
+++ Ejercicios/ProgramacionClase/E014TABLADEMULTIPLICAR.java
@@ -0,0 +1,25 @@
+package ProgramacionClase;
+
+public class E014TABLADEMULTIPLICAR {
+    void main() {
+        int numero = 0;
+        while (true) {
+            numero = Integer.parseInt(IO.readln("Introduce un numero para dar su tabla de multiplicar: "));
+
+            if (!(numero <= 0)) {
+                break;
+            }
+
+            IO.println("Escribe un numero positivo no decimal que no sea cero!! ");
+            IO.println();
+        }
+
+        for (int i = 1; i <= 10; i++) {
+            int resultado = i * numero;
+            System.out.format("%d x %d = %d \n", i, numero, resultado);
+        }
+
+        double raizCuadrada = Math.sqrt(numero);
+        System.out.format("√%d = %f", numero, raizCuadrada);
+    }
+}
