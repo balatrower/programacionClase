b9b21fd8a79f9a858a28f9587e4b8ad42d93a8ad
balatrower <power333xd@gmail.com>
2025-11-06 13:06:26 +0100

    E048 HECHO

---
+++ Ejercicios/ProgramacionClase/E048MEDIA.java
@@ -0,0 +1,25 @@
+package ProgramacionClase;
+
+public class E048MEDIA {
+    void main() {
+        int conadorNumeros = 0;
+        int sumaNumeros = 0;
+        int numeroDado = 1;
+        while (numeroDado != 0) {
+            try {
+                numeroDado = Integer.parseInt(IO.readln("Escribe numeros positivos para sacar su media, un 0 termina el programa: "));
+                ;
+
+                if (numeroDado > 0) {
+                    sumaNumeros += numeroDado;
+                    conadorNumeros++;
+                }
+            } catch (Exception enrique) {
+                IO.println("No se admite texto o numeros negativos.");
+            }
+        }
+
+        int media = sumaNumeros / conadorNumeros;
+        IO.println("La media de los numeros escritos es: " + media);
+    }
+}
