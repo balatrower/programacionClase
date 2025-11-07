73354a532705e820c1c3b983ae09d7e2270e7503
balatrower <power333xd@gmail.com>
2025-11-07 17:58:27 +0100

    E070 HECHO pero funca mal

---
+++ Ejercicios/ProgramacionClase/E070CONTARVOCALES.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+void main() {
+    String texto = IO.readln("Introduce un texto para contar cuantas vocales tiene: ");
+
+    IO.println("El texto: " + texto + " tiene: " + contarVocales(texto) + " vocales");
+}
+
+int contarVocales(String texto) {
+    int contadorVocales = 0;
+    String textoMinuscula = texto.toLowerCase();
+    Character[] arrayVocalesTilde = {'á', 'é', 'í', 'ó', 'ú'};
+    Character[] arrayVocales = {'a', 'e', 'i', 'o', 'u'};
+
+    for (int i = 0; i < texto.length(); i++) {
+        boolean esVocalConTilde = false;
+        boolean esVocal = false;
+        char c = textoMinuscula.charAt(i);
+
+        for (char caracterArray: arrayVocales) {
+            if (caracterArray == c) {
+                esVocal = true;
+                break;
+            }
+        }
+
+        for (char caracterArray: arrayVocalesTilde) {
+            if (caracterArray == c) {
+                esVocalConTilde = true;
+                break;
+            }
+        }
+
+        if (esVocal) {
+            contadorVocales++;
+        } else if (esVocalConTilde) {
+            contadorVocales++;
+        }
+    }
+
+    return contadorVocales;
+}
