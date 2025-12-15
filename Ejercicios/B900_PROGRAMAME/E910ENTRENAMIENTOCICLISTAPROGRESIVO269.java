a627d5fdefa4392420cf65ae0f259ad8653f6af5
balatrower <power333xd@gmail.com>
2025-12-16 00:56:02 +0100

    E910 HECHO Y SUBIDO

--- Ejercicios/B900_PROGRAMAME/E910ENTRENAMIENTOCICLISTAPROGRESIVO269.java
+++ Ejercicios/B900_PROGRAMAME/E910ENTRENAMIENTOCICLISTAPROGRESIVO269.java
@@ -7,6 +7,21 @@ public class E910ENTRENAMIENTOCICLISTAPROGRESIVO269 {
         Scanner entrada = new Scanner(System.in);
         int numCasos = entrada.nextInt();
 
+        for (int i = 0; i < numCasos; i++) {
+            int kilometrosTramoExtra = entrada.nextInt();
+            int acumuladoTotalKilometros = kilometrosTramoExtra;
+            int longitudRutaActual = kilometrosTramoExtra;
 
+            while (kilometrosTramoExtra != 0) {
+                kilometrosTramoExtra = entrada.nextInt();
+
+                if (kilometrosTramoExtra != 0) {
+                    longitudRutaActual = longitudRutaActual + kilometrosTramoExtra;
+                    acumuladoTotalKilometros = acumuladoTotalKilometros + longitudRutaActual;
+                }
+            }
+
+            System.out.println(acumuladoTotalKilometros * 2);
+        }
     }
 }
