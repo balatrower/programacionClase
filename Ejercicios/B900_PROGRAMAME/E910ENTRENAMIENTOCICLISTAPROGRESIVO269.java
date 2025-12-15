96524447c3dfcf63c80771678ffb1da4f571a492
balatrower <power333xd@gmail.com>
2025-12-15 14:06:12 +0100

    E910 creado

---
+++ Ejercicios/B900_PROGRAMAME/E910ENTRENAMIENTOCICLISTAPROGRESIVO269.java
@@ -0,0 +1,12 @@
+package B900_PROGRAMAME;
+
+import java.util.Scanner;
+
+public class E910ENTRENAMIENTOCICLISTAPROGRESIVO269 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int numCasos = entrada.nextInt();
+
+
+    }
+}
