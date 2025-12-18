c2d9ade6302a026ffe6abe194e07b4df8295259a
balatrower <power333xd@gmail.com>
2025-12-18 01:46:39 +0100

    E920 HECHO Y SUBIDO

---
+++ Ejercicios/B900_PROGRAMAME/E920CASTILLOSDENAIPES741.java
@@ -0,0 +1,16 @@
+package B900_PROGRAMAME;
+
+import java.util.Scanner;
+
+public class E920CASTILLOSDENAIPES741 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int numCasos = entrada.nextInt();
+
+        for (int i = 0; i < numCasos; i++) {
+            long numPisos = entrada.nextLong();
+            long totalCartas = (3 * (numPisos * numPisos) + numPisos) / 2;
+            System.out.println(totalCartas);
+        }
+    }
+}
