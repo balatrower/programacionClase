0aa3aebd42494c5a22e39cad61cab5777c1be54a
balatrower <power333xd@gmail.com>
2026-01-26 16:40:20 +0100

    E832 hecho

---
+++ Ejercicios/B800_VARIOS/E832FIBONNACIRECURSIVO.java
@@ -0,0 +1,21 @@
+package B800_VARIOS;
+
+public class E832FIBONNACIRECURSIVO {
+    void main() {
+        int numero = -1;
+        do {
+            numero = Integer.parseInt(IO.readln("Escribe numero positivo entero del que calcular su sucesion de fibonacci: "));
+        } while (numero < 0);
+        IO.println("El resultado de la succesion de fibonacci de " + numero + " es: " + fibonnaci(numero));
+    }
+
+    int fibonnaci(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fibonnaci(n - 1) + fibonnaci(n - 2);
+        }
+    }
+}
